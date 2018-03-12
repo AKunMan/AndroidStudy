d7ff7cae10637bc89fdcb239432601e8c3dce859
AKunMan <634125407@qq.com>
2018-03-12 17:09:45 +0800

    上传

--- app/src/main/java/com/example/administrator/activityjump/MainActivity.java
+++ app/src/main/java/com/example/administrator/activityjump/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.activityjump;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    public final static String CRAZZY_ACTION = "org.crazyit.intent.action.CRAZYIT_ACTION";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,11 +21,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        Bundle data = new Bundle();
-//        data.putSerializable("title_name","一本正经");
-        data.putString("title_name","一本正经");
-        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
-        intent.putExtras(data);
+        //第一种
+//        Bundle data = new Bundle();
+//        data.putString("title_name","一本正经");
+//        Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+//        intent.putExtras(data);
+//        startActivity(intent);
+
+        //ComponentName属性
+//        ComponentName componentName = new ComponentName(MainActivity.this,SecondActivity.class);
+//        Intent intent = new Intent();
+//        intent.setComponent(componentName);
+//        startActivity(intent);
+
+        //Action
+        Intent intent = new Intent();
+        intent.setAction(MainActivity.CRAZZY_ACTION);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/administrator/activityjump/SecondActivity.java
+++ app/src/main/java/com/example/administrator/activityjump/SecondActivity.java
@@ -1,5 +1,6 @@
 package com.example.administrator.activityjump;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,11 +18,22 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
         Button secBtn = (Button)findViewById(R.id.sec_at_btn);
         secBtn.setOnClickListener(this);
 
-        Intent intent = getIntent();
-        Bundle data = intent.getExtras();
-        String titleName = data.getString("title_name");
+        //第一种
+//        Intent intent = getIntent();
+//        Bundle data = intent.getExtras();
+//        String titleName = data.getString("title_name");
+//        TextView textView = (TextView)findViewById(R.id.sec_at_text);
+//        textView.setText(titleName);
+
+        //ComponentName属性
+//        ComponentName componentName = getIntent().getComponent();
+//        TextView textView = (TextView)findViewById(R.id.sec_at_text);
+//        textView.setText("组件包名:"+componentName.getPackageName()+"--------- 组件类名"+componentName.getClassName());
+
+        //Action
         TextView textView = (TextView)findViewById(R.id.sec_at_text);
-        textView.setText(titleName);
+        String str = getIntent().getAction();
+        textView.setText(str);
     }
 
     @Override
