e59ab85172cceca7c498d9cde60843ba7e5441ca
AKunMan <634125407@qq.com>
2018-03-29 15:55:18 +0800

    recycleView测试代码

--- app/src/main/java/com/example/administrator/activityjump/adapter/MyRecyclerAdapter.java
+++ app/src/main/java/com/example/administrator/activityjump/adapter/MyRecyclerAdapter.java
@@ -2,6 +2,7 @@ package com.example.administrator.activityjump.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +14,7 @@ import com.example.administrator.activityjump.R;
 import java.util.List;
 import java.util.Map;
 
-public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.MyViewHolder>{
+public class MyRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
 
     private List<Map<String, Object>> mDatas;
     private Context mContext;
@@ -27,36 +28,53 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
 
     @Override
     public int getItemCount() {
-
         return mDatas.size();
     }
 
-    //填充onCreateViewHolder方法返回的holder中的控件
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, final int position) {
-        Map map = mDatas.get(position);
-        holder.title_tv.setText((String)map.get("title"));
-        holder.logo.setImageResource((Integer) map.get("logo"));
-        holder.version_tv.setText((String)map.get("version"));
-        holder.size_tv.setText((String)map.get("size"));
-    }
 
     //重写onCreateViewHolder方法，返回一个自定义的ViewHolder
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View view = inflater.inflate(R.layout.item,parent, false);
-        MyViewHolder holder= new MyViewHolder(view);
-        return holder;
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d("recyclerlog","viewType:" + viewType);
+        if (viewType == 0){
+            View view = inflater.inflate(R.layout.item, parent, false);
+            MyViewHolder holder= new MyViewHolder(view);
+            return holder;
+        }else {
+            View view = inflater.inflate(R.layout.item2, parent, false);
+            MyViewHolder2 holder= new MyViewHolder2(view);
+            return holder;
+        }
     }
 
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Map map = mDatas.get(position);
+        if (position == 10){
+            MyViewHolder2 myHolder2 = (MyViewHolder2)holder;
+            myHolder2.title_tv.setText((String)map.get("title"));
+        }else {
+            MyViewHolder myHolder1 = (MyViewHolder)holder;
+            myHolder1.title_tv.setText((String)map.get("title"));
+            myHolder1.logo.setImageResource((Integer) map.get("logo"));
+            myHolder1.version_tv.setText((String)map.get("version"));
+            myHolder1.size_tv.setText((String)map.get("size"));
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 10){
+            return 1;
+        }
+        return 0;
+    }
     class MyViewHolder extends RecyclerView.ViewHolder {
 
         TextView title_tv;
         ImageView logo;
         TextView version_tv;
         TextView size_tv;
-
 
         public MyViewHolder(View view) {
             super(view);
@@ -65,6 +83,12 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.My
             version_tv = (TextView)view.findViewById(R.id.itme_version);
             size_tv = (TextView)view.findViewById(R.id.itme_size);
         }
-
+    }
+    class MyViewHolder2 extends RecyclerView.ViewHolder{
+        TextView title_tv;
+        public MyViewHolder2(View itemView) {
+            super(itemView);
+            title_tv=itemView.findViewById(R.id.title2);
+        }
     }
 }
