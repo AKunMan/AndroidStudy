248591d7b9ebaf5d1e73869675c4cd43df9c741b
AKunMan <634125407@qq.com>
2018-04-02 16:37:23 +0800

    shangchuan

--- app/src/main/java/com/example/administrator/activityjump/SecondActivity.java
+++ app/src/main/java/com/example/administrator/activityjump/SecondActivity.java
@@ -5,11 +5,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity implements View.OnClickListener{
 
+    ImageView imageView;
+    Animation anim;
+    Animation reverse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +37,25 @@ public class SecondActivity extends AppCompatActivity implements View.OnClickLis
 //        textView.setText("组件包名:"+componentName.getPackageName()+"--------- 组件类名"+componentName.getClassName());
 
         //Action
-        TextView textView = (TextView)findViewById(R.id.sec_at_text);
-        String str = getIntent().getAction();
-        textView.setText(str);
+//        TextView textView = (TextView)findViewById(R.id.sec_at_text);
+//        String str = getIntent().getAction();
+//        textView.setText(str);
+
+        this.loadAnimat();
     }
 
+    private void loadAnimat(){
+        imageView = (ImageView)findViewById(R.id.sec_img);
+        //加载第一个动画
+        anim = AnimationUtils.loadAnimation(this,R.anim.anim);
+        anim.setFillAfter(true);
+        //加载第二个动画
+        reverse = AnimationUtils.loadAnimation(this,R.anim.reverse);
+        reverse.setFillAfter(true);
+    }
     @Override
     public void onClick(View view) {
-        finish();
+        imageView.startAnimation(anim);
+//        finish();
     }
 }
